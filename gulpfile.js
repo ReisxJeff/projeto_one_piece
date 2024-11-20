9bf5bd25ca9560ecf93ef5e2ac325e58b8a7c0f0
Jefferson Reis <95jefferson.reis@gmail.com>
2024-11-20 16:55:51 -0300

    configurando uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 // Tarefa para compilar os arquivos .scss em CSS e minificar
 function styles() {
@@ -18,7 +25,7 @@ function images() {
 
 // Tarefa build: compila CSS e otimiza imagens
 // A tarefa build é a padrão e será executada quando rodar `gulp` sem argumentos
-exports.build = gulp.parallel(styles, images);  // Compila os estilos e otimiza as imagens em paralelo
+exports.build = gulp.parallel(styles, images, scripts);  // Compila os estilos e otimiza as imagens em paralelo
 
 // Tarefa watch: monitora alterações nos arquivos .scss e nas imagens
 // Quando houver alterações, as respectivas tarefas serão executadas novamente
@@ -28,6 +35,7 @@ exports.watch = function() {
 
     // Monitora todas as imagens e executa a tarefa 'images' quando houver alteração
     gulp.watch('./src/images/**/*', gulp.series(images));
+    gulp.watch('./src/scripts/*.js', gulp.series(scripts));
 };
 
 // A tarefa default será o build, portanto, executada quando rodar `gulp` sem argumentos
